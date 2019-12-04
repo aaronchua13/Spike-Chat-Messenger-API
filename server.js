fef74f041c2dddbede77e1a28289369cd8be3c40
AC <caaron@dnamicro.com>
2019-12-04 20:01:06 +0800

    with upload

--- server.js
+++ server.js
@@ -3,16 +3,24 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const http = require('http')
+const bodyParser = require('body-parser')
+const path = require('path');
 // server instance
 const server = http.createServer(app)
 const socketIO = require('socket.io');
 const fetch = require('node-fetch')
 const cors = require('cors')
+const fs =require('fs', {
+  withFileTypes: true
+})
+const ss = require('socket.io-stream')
 
 // creates socket using the instance of the server
 const io = socketIO(server, { wsEngine: 'ws' })
 
 app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // get users
 app.get('/user', async (req, res) => {
@@ -45,6 +53,53 @@ io.on('connection', socket => {
     }
   })
 
+  ss(socket).on('all_images', () => {
+    const fileTypes = ['.jpg', '.jpeg', '.png']
+    const directoryPath = path.join(__dirname, 'files');
+
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    }
+  
+    files.filter(e => 
+      fileTypes.includes(
+        e.slice(
+            e.lastIndexOf('.')
+          )
+      )
+    ).map(e => {
+        const outboundStream = ss.createStream()
+        ss(socket).emit('all_images', outboundStream, {
+          file_name: e
+        })
+        fs.createReadStream(path.join('./files', e)).pipe(outboundStream)
+      })
+    });
+  })
+  
+  ss(socket).on('upload_image', (stream, file) => {
+      var filename = path.basename(file.file_name);
+      const filepath = path.join('./files', filename)
+      stream.pipe(fs.createWriteStream(filepath));
+      let size = 0
+
+      stream.on('data', (chunk) => {
+        size += chunk.length;
+        const percent = Math.floor(size / file.size * 100)
+        console.log(`${percent === 100 ? 'Upload Complete' : `Uploading... ${percent}%`}`);
+      });
+
+      const outboundStream = ss.createStream()
+      stream.on('end', () => {
+          ss(socket).emit('receive_client', outboundStream, {
+            size: file.size,
+            file_name: file.file_name
+          })
+          fs.createReadStream(filepath).pipe(outboundStream)
+      });
+  });
+  
   socket.on('disconnect', () => {
     console.log('User Disconnected')
   })
